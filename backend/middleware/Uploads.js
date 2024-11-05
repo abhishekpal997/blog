4662ea57d15893ab0bec3aa5a7d71ca2237c995f
abhishekpal997 <abhishek.ibpwebtech@gmail.com>
2024-11-05 11:36:57 +0530

    Update Uploads.js

--- backend/middleware/Uploads.js
+++ backend/middleware/Uploads.js
@@ -4,7 +4,7 @@ const path = require('path');
 // Configure storage for user uploads
 const userStorage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, "/uploads/user");
+        cb(null, "./uploads/user");
     },
     filename: (req, file, cb) => {
         cb(null, Date.now() + path.extname(file.originalname));
